5f166147d2284434459219706fe2b0a2e9230c1f
Chris Devereux <chrisd(a)damplus.com>
2016-09-30 10:43:37 +0100

    Remove value prop from button

--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -2,10 +2,9 @@ import * as React from 'react'
 import * as css from 'classnames'
 const shallowCompare = require('react-addons-shallow-compare')
 
-export interface ButtonProps<T> {
+export interface ButtonProps {
   theme: ButtonTheme
-  value: T
-  onClick: (x: T, e: React.SyntheticEvent<{}>) => void
+  onClick: (e: React.SyntheticEvent<{}>) => void
   iconClass?: string
 }
 
@@ -19,26 +18,18 @@ export interface ButtonTheme {
   text: string
 }
 
-export default class Button<T> extends React.Component<ButtonProps<T>, ButtonState> {
+export default class Button extends React.Component<ButtonProps, ButtonState> {
   state: ButtonState = {}
 
-  componentWillMount() {
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  shouldComponentUpdate(newProps: ButtonProps<T>, newState: ButtonState) {
+  shouldComponentUpdate(newProps: ButtonProps, newState: ButtonState) {
     return shallowCompare(this, newProps, newState)
-  }
-
-  handleClick(e: React.SyntheticEvent<{}>) {
-    this.props.onClick(this.props.value, e)
   }
 
   render() {
     const { theme, iconClass, children } = this.props
 
     return (
-      <button className={theme.root} onClick={this.handleClick}>
+      <button className={theme.root} onClick={this.props.onClick}>
         <span className={theme.text}>{children}</span>
         {
           iconClass && (
