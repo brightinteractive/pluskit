3052687a6db57178b1cff7b4a0786bdbca6f6b29
Chris Devereux <chrisd(a)damplus.com>
2016-11-10 15:28:13 +0000

    Add @damplus/app dependency

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,9 @@
-import { configure } from '@kadira/storybook';
-import 'normalize.css'
+const { configure } = require('@kadira/storybook')
+require('normalize.css')
 const req = require.context('../packages', true, /examples\.tsx$/)
 
 function loadStories() {
-  console.log('keys', req.keys())
+  console.log(req.keys())
   req.keys().forEach(req)
 }
 
